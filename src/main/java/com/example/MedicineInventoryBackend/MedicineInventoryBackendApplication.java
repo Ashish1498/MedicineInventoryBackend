7efabab7531deec6b2f84231ff09170ef871e08e
Utkarsh Ashish <utkarshashish@Utkarshs-MacBook-Pro.local>
2021-03-02 18:09:16 +0530

    boiler plate

---
+++ src/main/java/com/example/MedicineInventoryBackend/MedicineInventoryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.MedicineInventoryBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicineInventoryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicineInventoryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MedicineInventoryBackend/MedicineInventoryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MedicineInventoryBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicineInventoryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
